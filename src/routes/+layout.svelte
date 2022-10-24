7a0afaa87bd6d5a0814f10297c1e2a07a8c448ac
Jacob-WAC <johnsondljl7@yahoo.com>
2022-10-24 17:08:16 -0500

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+
+</script>
+<nav>
+    <a href="/"><h1>J -> J</h1></a>
+    <a href="/settings"><p>Information</p></a>
+</nav>
+
+    <slot></slot>
+
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap');
+
+    nav {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: rgba(209, 209, 209, 0);
+        position: fixed;
+        top: 0;
+        width: 100%;
+
+        a {
+            margin-top: 2vw;
+            margin-left: 2vw;
+            margin-right: 2vw;
+            text-decoration: none;
+            color: black; 
+            font-family: 'Quicksand', sans-serif;
+
+            :visited {
+                color: black;
+                text-decoration: none;
+                    }           
+            
+            h1{
+            font-size:2.5em;
+            
+            }
+
+            p{
+            font-size: 1.5em;
+            margin-top: .2vw;
+            
+            
+            }
+
+            }
+
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+
+<script lang="ts">
+  
+</script>
+
+<div>
+<h1>Welcome to SvelteKitAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
+
+<style lang="scss">
+
+div{
+margin-top: 5vw;
+margin-left: 12%;
+margin-right: 12%;
+word-wrap: break-word;
+}
+
+
+</style>
